cdeccb178ca5d881f07d1133de70c693a1532ddc
yoon gyu heon <ykh9871@gmail.com>
2023-06-23 19:57:49 +0900

    Add request classes for user-related operations
    사용자 관련 작업을 위한 요청 클래스 추가

---
+++ src/main/java/com/ykh/backend/request/Requests.java
@@ -0,0 +1,31 @@
+package com.ykh.backend.request;
+
+public class Requests {
+    public static class DeleteUserRequest {
+        public String password;
+    }
+
+    public static class FindPasswordRequest {
+        public String phoneNumber;
+        public String email;
+    }
+
+    public static class PasswordChangeRequest {
+        public String currentPassword;
+        public String newPassword;
+    }
+
+    public static class PhoneNumberRequest {
+        public String phoneNumber;
+    }
+
+    public static class VerifyEmailRequest {
+
+        public String email;
+        public String code;
+    }
+    public static class PasswordResetRequest{
+        public String token;
+        public String newPassword;
+    }
+}
