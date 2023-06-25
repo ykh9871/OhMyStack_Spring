a3910ddf074857bd08cc9a860dbf68e495eeb337
yoon gyu heon <ykh9871@gmail.com>
2023-06-26 05:03:15 +0900

    WebConfig 클래스에서 CORS 설정 구성

--- src/main/java/com/ykh/backend/config/WebConfig.java
+++ src/main/java/com/ykh/backend/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.ykh.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Component
 public class WebConfig implements WebMvcConfigurer {
 
     // CORS 설정
